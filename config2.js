4daa027e6b660a0ca643021e4d542d5ce24a471c
gworkhub <150426042+gworkhub@users.noreply.github.com>
2023-12-12 16:29:28 +0530

    Initial commit

---
+++ config2.js
@@ -0,0 +1,12 @@
+var config2 = {};
+
+config2.endpoint = 'https://plantdata.documents.azure.com:443/';
+config2.key = '9dRts4FuCaunXOZmFP1RbRBAkWUk68Kp4MkU8M2aWabLazVUhYwPkLRqmIsEzIKSYtks9qOibhvMACDbI8wmyQ==';
+config2.database = {
+    id: 'ToDoList'
+};
+config2.container = {
+    id: 'Items'
+};
+
+module.exports = config2;
